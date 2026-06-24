44537c2a444b200da20f5bbaf7e188ce0ad110cf
Guilherme Cotrim <guisobiso@gmail.com>
2026-06-24 18:26:39 -0300

    lab6 - minor fix

--- Lab6/src/Ex2/Main.java
+++ Lab6/src/Ex2/Main.java
@@ -1,11 +1,11 @@
 package Ex2;
 
-import Ex1.Cafe;
-import Ex1.Cappucino;
-import Ex1.Cha;
-
 public class Main {
     public static void main(String[] args){
+        // diferença entre Template e Decorator: O Template define a estrutura básica de um algoritmo numa classe,
+        // permitindo que partes sejam alteradas pelas subclasses. O Decorator envolve objetos para adicionar ou
+        // modificar funcionalidades dinamicamente, sem alterar a classe original;
+
         Online pedido_online = new Online();
         Loja pedido_loja = new Loja();
         Cripto pedido_cripto = new Cripto();
